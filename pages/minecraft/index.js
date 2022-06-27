8b3f759476e4bbe7bbe27a4d3faa7e9451213a74
bezalel6 <bezalel3250@gmail.com>
2022-06-27 20:07:27 +0300

    THE CUBE IS WORKING

--- pages/minecraft/index.js
+++ pages/minecraft/index.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from "react";
 import { init } from "pages/_app.js";
+// import CleanUp from "./script.js";
 
 export default function Minecraft() {
   useEffect(() => {
     init();
-    require("./script.js");
+    var clean = require("./script.js");
+    // clean();
+    console.log("%cindex.js line:10 clean", "color: #007acc;", clean.default);
+    return () => console.log("this is returning");
   }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     console.log("wow this shit");
+  //   };
+  // });
   return (
     <>
       <canvas id="canvas1"></canvas>
--- pages/minecraft/script.js
+++ pages/minecraft/script.js
@@ -8,9 +8,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const scale = 1;
-
+const perspective = 1400;
 const convertTo2d = (obj, width = 1920, height = 1080) => {
-  console.log("%cscript.js line:13 obj", "color: #007acc;", obj);
   const { x, y, z } = obj;
   const x3d = y * scale;
   const y3d = z * scale;
@@ -22,7 +21,7 @@ const scaleFunc = (x3d, y3d, depth) => {
   let dist = Math.sqrt(x3d * x3d + y3d * y3d);
   const theta = Math.atan2(y3d, x3d);
   const depth2 = 15 - depth; //relative to the camera. number of x points away from the camera
-  const localScale = Math.abs(1400 / (depth2 + 1400));
+  const localScale = Math.abs(perspective / (depth2 + perspective));
   dist *= localScale;
   const x = dist * Math.cos(theta);
   const y = dist * Math.sin(theta);
@@ -32,7 +31,6 @@ const clrs = ["green", "yellow", "blue", "red", "pink", "cyan"];
 let index = 0;
 class Polygon {
   constructor(points, color = clrs[index++]) {
-    console.log("%cscript.js line:35 points", "color: #007acc;", points);
     this.points = points;
     this.color = color;
   }
@@ -51,37 +49,44 @@ class Polygon {
     ctx.closePath();
     ctx.fill();
   }
+  getAvgX() {
+    let sum = 0;
+    this.points.forEach((p) => (sum += p.x));
+    return sum / this.points.length;
+  }
 
   rotate(degX, degY, degZ, cw = true) {
     points.forEach((point) => {
       rotateX(point, degX, cw);
-      // rotateY(point, degY, cw);
-      // rotateZ(point, degZ, cw);
+      rotateY(point, degY, cw);
+      rotateZ(point, degZ, cw);
     });
   }
 }
 const rotateX = (p, deg, cw) => {
-  console.log("b4", p);
-  const radius = Math.sqrt(p.y * p.y + p.z * p.z);
+  let radius = Math.sqrt(p.y * p.y + p.z * p.z);
+  // radius = strip(radius);
   let theta = Math.atan2(p.z, p.y);
+  // theta = strip(theta);
   theta += ((2 * Math.PI) / 360) * deg * (cw ? -1 : 1);
-  p.y = radius * Math.cos(theta);
-  p.z = radius * Math.sin(theta);
-  console.log("after", p);
+  // theta = strip(theta);
+  p.y = strip(radius * Math.cos(theta));
+  p.z = strip(radius * Math.sin(theta));
 };
 const rotateY = (p, deg, cw) => {
-  const radius = Math.sqrt(p.y * p.y + p.z * p.z);
-  let theta = Math.atan2(p.y, p.z);
-  theta += ((2 * Math.PI) / 360) * deg * (cw ? 1 : -1);
-  p.y = radius * Math.cos(theta);
-  p.z = radius * Math.sin(theta);
+  const radius = Math.sqrt(p.x * p.x + p.z * p.z);
+  let theta = Math.atan2(p.x, p.z);
+  // theta = strip(theta);
+  theta += ((2 * Math.PI) / 360) * deg * (cw ? -1 : 1);
+  p.x = radius * Math.sin(theta);
+  p.z = radius * Math.cos(theta);
 };
 const rotateZ = (p, deg, cw) => {
-  const radius = Math.sqrt(p.y * p.y + p.z * p.z);
-  let theta = Math.atan2(p.y, p.z);
-  theta += ((2 * Math.PI) / 360) * deg * (cw ? 1 : -1);
-  p.y = radius * Math.cos(theta);
-  p.z = radius * Math.sin(theta);
+  const radius = Math.sqrt(p.y * p.y + p.x * p.x);
+  let theta = Math.atan2(p.y, p.x);
+  theta += ((2 * Math.PI) / 360) * deg * (cw ? -1 : 1);
+  p.y = radius * Math.sin(theta);
+  p.x = radius * Math.cos(theta);
 };
 class Tetrahedron {
   constructor(polygons) {
@@ -92,7 +97,14 @@ class Tetrahedron {
   }
   rotate(degX, degY, degZ, cw = true) {
     this.polygons.forEach((poly) => poly.rotate(degX, degY, degZ, cw));
+    this.sort();
   }
+  sort() {
+    this.polygons = this.polygons.sort((a, b) => a.getAvgX() - b.getAvgX());
+  }
+}
+function strip(number) {
+  return parseFloat(number).toPrecision(12);
 }
 // create all 8 points of a cube
 const s = 100;
@@ -121,5 +133,17 @@ let polygons = [
 ];
 polygons = polygons.map((p) => new Polygon(p));
 const tetra = new Tetrahedron(polygons);
-tetra.rotate(300, 0, 0);
-tetra.draw(ctx);
+
+if (localStorage.lastId) {
+  clearInterval(localStorage.lastId);
+}
+
+localStorage.lastId = setInterval(() => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  tetra.draw(ctx);
+  tetra.rotate(1, 1, 0);
+}, 100);
+
+export default function CleanUp() {
+  console.log("cleanup crew");
+}
