40e92e61c5b882294e7e09f7708a92d7952c1a28
bezalel6 <bezalel3250@gmail.com>
2022-07-06 16:31:06 +0300

    hesfko

--- pages/knight/index.js
+++ pages/knight/index.js
@@ -10,15 +10,10 @@ export default function Knight() {
 
   return (
     <>
-      <h1>
-        this is the only game i actually put some time into. it took me like a
-        day, and most of it was fighting with react (yes. this is me flexing)
-      </h1>
-      <br />
       <iframe
         src="https://www.funnyhowtheknightmoves.com/"
-        width="90%"
-        height="90%"
+        // width="90%"
+        // height="90%"
         className="fullscreen"
         id="frame"
       ></iframe>
--- pages/tic-tac-toe/index.js
+++ pages/tic-tac-toe/index.js
@@ -4,20 +4,21 @@ import { useEffect } from "react";
 
 export default function Knight() {
   useEffect(() => {
-    $("#frame").contents().find("head link, style").remove();
+    // $("#frame").contents().find("head link, style").remove();
     init(undefined, undefined, {
-      header:
-        "i made this in like half a day. most of which was fighting with react. ('weird flex but ok')",
+      header: "",
     });
   }, []);
 
   return (
     <>
+      this is the game i put the most time into.. like 4 hours. ('weird flex but
+      ok')
       <iframe
         src="https://my-tic-tac-toe-ai.netlify.app//"
         width="100%"
         height="100%"
-        className="fullscreen"
+        className="most-fullscreen"
         id="frame"
       ></iframe>
     </>
